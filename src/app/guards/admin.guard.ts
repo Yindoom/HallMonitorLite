ce64c42f976a91620e3544cdba6d518d61de774a
Yindo <bast0099@easv365.dk>
2019-12-10 10:35:37 +0100

    Admin Guard reroute change

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -10,7 +10,7 @@ export class AdminGuard implements CanActivate {
     if (this.authService.isAdmin()) {
       return true;
     } else {
-      this.router.navigate(['/login']);
+      this.router.navigate(['/dashboard']);
       return false;
     }
   }
